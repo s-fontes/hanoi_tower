4bac4d359daa25ed987f6e56f2f385d6f6bd1134
Sérgio Fontes <100804634+s-fontes@users.noreply.github.com>
2023-09-12 19:29:59 +0000

    add execution time

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from time import time
+
 from hanoi_tower import HanoiTower
 from output_controller import OutputController
 
 if __name__ == "__main__":
     output_controller = OutputController()
     tower = HanoiTower(5, output_controller)
+    initial_time = time()
     tower.solve()
+    output_controller.write("=====================================")
+    output_controller.write("Execution time: %.4f" % (time() - initial_time))
+
 
